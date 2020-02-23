da25984df41efabcc0dec64a3b3c920137214b84
Alicia Key <alicia@akey7.com>
2020-02-22 22:08:26 -0700

    Issue #12
    This places the parameters as properties of the edges.

--- attempt_2/mol_dyn_md.py
+++ attempt_2/mol_dyn_md.py
@@ -24,11 +24,22 @@ atom_masses = {
 SymbolPositionVelocity = namedtuple("SymbolPositionVelocity", ("symbol", "position", "velocity"))
 
 
+# See docstring for add_bond below and the citations above for more explnation
+# of these parameters
+Bond = namedtuple("Bond", ("l_IJ_0", "k_IJ"))
+
+
 class MolDynMD:
     """
     This is a molecular dynamics class. It only supports stretch energy
 
     Use MKS units for everything here and life will be easier.
+
+    The nodes on the graph store the indecies into which the atom types
+    can be found, as well as the positions and velocities (the latter two
+    being store in NumPy arrays)
+
+    The edges on the graph store the stretch energy parameters for the bond.
     """
 
     def __init__(self):
@@ -107,18 +118,50 @@ class MolDynMD:
 
         return index_for_new_atom
 
-    def add_bond(self, atom1, atom2, l_IJ, k_IJ):
-        """
-        This adds a bond as an edge on the graph.
-
-        Parameters
-        ----------
-        atom1 : int
-            The index of the first atom to draw the bond to
-        """
-        pass
-
     @property
     def symbols_positions_velocities(self):
         result = [SymbolPositionVelocity(s, p, v) for s, p, v in zip(self.symbols, self.positions, self.velocities)]
         return result
+
+    def add_bond(self, atom1, atom2, l_IJ_0, k_IJ):
+        """
+        This adds a bond as an edge on the graph.
+
+        This is an undirected graph, so we don't need to add edges going in
+        both directions.
+
+        Parameters
+        ----------
+        atom1 : int
+            Index of the first atom in the bond.
+
+        atom2 : int
+            Index of the second atom in the bond
+
+        l_IJ_0 : float
+            Reference length of the bond. See the Levine citation above.
+
+        k_IJ : float
+            Force constant of the bond.
+
+        Raises
+        ------
+        ValueError
+            Raised if the force constant >= 0, or if the reference length is
+            negative. Also raised if atom1 or atom2 point to non existent atoms
+        """
+        if atom1 > len(self.symbols) - 1:
+            raise ValueError(f"atom1 {atom1} is out of range")
+
+        if atom2 > len(self.symbols) - 1:
+            raise ValueError(f"atom2 {atom2} is out of range")
+
+        if l_IJ_0 <= 0:
+            raise ValueError(f"l_IJ_0 of {l_IJ_0 } must be greater than or equal to 0")
+
+        if k_IJ >= 0:
+            raise ValueError(f"k_IJ of {k_IJ} should be negative")
+
+        bond = Bond(l_IJ_0=l_IJ_0, k_IJ=k_IJ)
+
+        self.graph.add_edge(atom1, atom2, bond=bond)
--- attempt_2/test/test_mol_dyn_md_h2.py
+++ attempt_2/test/test_mol_dyn_md_h2.py
@@ -5,8 +5,16 @@ import pytest
 from mol_dyn_md import MolDynMD, SymbolPositionVelocity
 
 
+bond_length_of_H2_m = 74.0e-12
+force_constant = -1.2
+
+
 @pytest.fixture()
 def h2_md_initial_state():
+    """
+    Set up an H2 molecule to do all the tests.
+    """
+
     md = MolDynMD()
     h1_initial_position = np.array([-32e-12, 0, 0])
     h2_initial_position = np.array([32e-12, 0, 0])
@@ -14,6 +22,7 @@ def h2_md_initial_state():
     h2_initial_velocity = np.array([0, 0, 0])
     h1 = md.add_atom("H", initial_position=h1_initial_position, initial_velocity=h1_initial_velocity)
     h2 = md.add_atom("H", initial_position=h2_initial_position, initial_velocity=h2_initial_velocity)
+    md.add_bond(h1, h2, l_IJ_0=bond_length_of_H2_m, k_IJ=force_constant)
     return md
 
 
@@ -24,18 +33,29 @@ def test_h2_positions(h2_md_initial_state):
 
 
 def test_h2_velocities(h2_md_initial_state):
+    """
+    Test the positions that live outside the graph
+    """
     expected_positions = np.array([[0, 0, 0], [0, 0, 0]])
     actual_positions = h2_md_initial_state.velocities
     assert np.all(expected_positions == actual_positions)
 
 
 def test_h2_nodes(h2_md_initial_state):
+    """
+    Make sure that all the expected nodes have been set.
+    """
+
     expected_nodes = {0, 1}
     actual_nodes = set(h2_md_initial_state.graph.nodes())
     assert expected_nodes == actual_nodes
 
 
 def test_symbol_positions_velocities(h2_md_initial_state):
+    """
+    Ensure the symbols, positions and their velocities are where they should be
+    """
+
     expected_symbol_positions_velocities = [
         SymbolPositionVelocity("H", np.array([[-32e-12, 0, 0]]), np.array([0, 0, 0])),
         SymbolPositionVelocity("H", np.array([[32e-12, 0, 0]]), np.array([0, 0, 0]))
@@ -44,3 +64,21 @@ def test_symbol_positions_velocities(h2_md_initial_state):
 
     for (s1, p1, v1), (s2, p2, v2) in zip(expected_symbol_positions_velocities, actual_symbol_positions_velocities):
         assert s1 == s2 and np.all(p1 == p2) and np.all(v1 == v2)
+
+
+def test_presence_of_edges(h2_md_initial_state):
+    """
+    Since this is a diatomic molecule and it is on an undirected graph,
+    there is only one edge to test.
+    """
+
+    assert h2_md_initial_state.graph.has_edge(0, 1)
+
+
+def test_edge_parameters(h2_md_initial_state):
+    """
+    This tests to make sure the edge contains the parameters that it should.
+    """
+
+    bond = h2_md_initial_state.graph.get_edge_data(0, 1)["bond"]
+    assert bond.l_IJ_0 == bond_length_of_H2_m and bond.k_IJ == force_constant
