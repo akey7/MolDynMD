2bedb42f4126ff415753598ed027daf21b40fa0f
Alicia Key <alicia@akey7.com>
2020-02-22 23:24:21 -0700

    Issue #17
    Test the bond length storage.

--- attempt_2/test/test_mol_dyn_md_h2.py
+++ attempt_2/test/test_mol_dyn_md_h2.py
@@ -5,7 +5,7 @@ import pytest
 from mol_dyn_md import MolDynMD
 
 
-bond_length_of_HCl_m = 127.45e-12
+reference_length_of_HCl_m = 127.45e-12
 force_constant = -1.0
 
 
@@ -16,19 +16,24 @@ def hcl():
     """
 
     md = MolDynMD()
+    h_initial_position = np.array([reference_length_of_HCl_m, 0, 0])
     cl_initial_position = np.array([0, 0, 0])
-    h_initial_position = np.array([bond_length_of_HCl_m, 0, 0])
     h_initial_velocity = np.array([0, 0, 0])
     cl_initial_velocity = np.array([0, 0, 0])
     h1 = md.add_atom("H", initial_position=h_initial_position, initial_velocity=h_initial_velocity)
     cl = md.add_atom("Cl", initial_position=cl_initial_position, initial_velocity=cl_initial_velocity)
-    md.add_bond(h1, cl, l_IJ_0=bond_length_of_HCl_m, k_IJ=force_constant)
+    md.add_bond(h1, cl, l_IJ_0=reference_length_of_HCl_m, k_IJ=force_constant)
     return md
 
 
-def test_h2_atom_count(hcl):
+def test_HCl_atom_count(hcl):
     assert len(hcl.graph.nodes) == 2
 
 
-def test_h2_atom_types(hcl):
+def test_HCl_atom_types(hcl):
     assert hcl.graph.nodes[0]["symbol"] == "H" and hcl.graph.nodes[1]["symbol"] == "Cl"
+
+
+def test_HCl_reference_length(hcl):
+    bond = hcl.graph.edges[0, 1]
+    assert bond["l_IJ_0"] == reference_length_of_HCl_m
