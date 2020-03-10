a4f0f2a28ad5ea76d331581278566f7582d36379
Alicia Key <alicia@akey7.com>
2020-03-09 20:59:12 -0600

    Update co2_trajectory.py
    This at least get some oscillatory motion that does not go crazy.

--- co2_trajectory.py
+++ co2_trajectory.py
@@ -3,24 +3,24 @@ import numpy as np
 from mol_dyn_md import MolDynMD
 
 """
-All distances are in picometers.
+All distances are in angstroms.
 
 All times are in picoseconds
 """
 
 md = MolDynMD(timesteps=1000, dt=1e-3)
 
-co2_bond_length = 116.
+co2_bond_length = 1.16
 force_constant = -1
 
 c_initial_position = np.array([0., 0., 0.])
 c_initial_velocity = np.array([0., 0., 0.])
 
 o1_initial_position = np.array([co2_bond_length * 0.99, 0., 0.])
-o1_initial_velocity = np.array([0., 0., 0.])
+o1_initial_velocity = np.array([-0.1, 0., 0.])
 
 o2_initial_position = np.array([-co2_bond_length * 0.99, 0., 0.])
-o2_initial_velocity = np.array([0., 0., 0.])
+o2_initial_velocity = np.array([0.1, 0., 0.])
 
 c = md.add_atom("C", initial_position=c_initial_position, initial_velocity=c_initial_velocity)
 o1 = md.add_atom("O", initial_position=o1_initial_position, initial_velocity=o1_initial_velocity)
@@ -33,7 +33,7 @@ md.run()
 
 filename = os.path.join("xyz", "CO2 Trajectory.xyz")
 with open(filename, "w") as f:
-    frames = md.trajectory_to_xyz_frames(scaling_factor=1e-10)
+    frames = md.trajectory_to_xyz_frames(scaling_factor=1)
     print("\n".join(frames), file=f)
 
 filename = os.path.join("xyz", "CO2 Tracjectory.csv")
