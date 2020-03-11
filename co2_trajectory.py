4b68947953b5fda2150b3fe7c712663b859a9bdc
Alicia Key <alicia@akey7.com>
2020-03-10 19:50:19 -0600

    Issue #41
    Adds a second CO2

--- co2_trajectory.py
+++ co2_trajectory.py
@@ -8,13 +8,14 @@ All distances are in angstroms.
 All times are in picoseconds
 """
 
-md = MolDynMD(timesteps=250000, dt=1e-3)
+md = MolDynMD(timesteps=1000, dt=1e-3)
 
 co2_bond_length = 1.16
 force_constant = -1
 
-c_initial_position = np.array([0., 0., 0.])
-c_initial_velocity = np.array([0., 0., 0.])
+# First CO2 molecule
+c_1_initial_position = np.array([0., 0., 0.])
+c_1_initial_velocity = np.array([0., 0., 0.])
 
 o1_initial_position = np.array([co2_bond_length, 0., 0.])
 o1_initial_velocity = np.array([-0.01, 0., 0.])
@@ -22,12 +23,29 @@ o1_initial_velocity = np.array([-0.01, 0., 0.])
 o2_initial_position = np.array([-co2_bond_length, 0., 0.])
 o2_initial_velocity = np.array([0.01, 0., 0.])
 
-c = md.add_atom("C", initial_position=c_initial_position, initial_velocity=c_initial_velocity)
+c1 = md.add_atom("C", initial_position=c_1_initial_position, initial_velocity=c_1_initial_velocity)
 o1 = md.add_atom("O", initial_position=o1_initial_position, initial_velocity=o1_initial_velocity)
 o2 = md.add_atom("O", initial_position=o2_initial_position, initial_velocity=o2_initial_velocity)
 
-md.add_bond(c, o1, l_IJ_0=co2_bond_length, k_IJ=force_constant)
-md.add_bond(c, o2, l_IJ_0=co2_bond_length, k_IJ=force_constant)
+md.add_bond(c1, o1, l_IJ_0=co2_bond_length, k_IJ=force_constant)
+md.add_bond(c1, o2, l_IJ_0=co2_bond_length, k_IJ=force_constant)
+
+# Second CO2 molecule
+c_2_initial_position = np.array([0., 0., co2_bond_length * 3])
+c_2_initial_velocity = np.array([0., 0., 0.])
+
+o3_initial_position = np.array([co2_bond_length, 0., co2_bond_length * 3])
+o3_initial_velocity = np.array([-0.01, 0., 0.])
+
+o4_initial_position = np.array([-co2_bond_length, 0., co2_bond_length * 3])
+o4_initial_velocity = np.array([0.01, 0., 0.])
+
+c2 = md.add_atom("C", initial_position=c_2_initial_position, initial_velocity=c_2_initial_velocity)
+o3 = md.add_atom("O", initial_position=o3_initial_position, initial_velocity=o3_initial_velocity)
+o4 = md.add_atom("O", initial_position=o4_initial_position, initial_velocity=o4_initial_velocity)
+
+md.add_bond(c2, o3, l_IJ_0=co2_bond_length, k_IJ=force_constant)
+md.add_bond(c2, o4, l_IJ_0=co2_bond_length, k_IJ=force_constant)
 
 md.run()
 
